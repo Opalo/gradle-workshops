c473d397c74998243d7ac60d57b6dd281d806503
Maciej Opala <maciek.opala@gmail.com>
2013-09-29 15:33:05 +0200

    ex_14

---
+++ beginner/ex_14/src/test/groovy/org/warsjawa/gradleWS/ExSpec.groovy
@@ -0,0 +1,16 @@
+package org.warsjawa.gradleWS
+
+import spock.lang.Specification
+
+class ExSpec extends Specification {
+
+    def 'success test'() {
+        expect:
+        1
+    }
+
+    def 'fail test'() {
+        expect:
+        null
+    }
+}
---
+++ beginner/ex_14/src/test/groovy/org/warsjawa/gradleWS/SolutionSpec.groovy
@@ -0,0 +1,43 @@
+package org.warsjawa.gradleWS
+
+import org.gradle.tooling.ProjectConnection
+import spock.lang.Shared
+import spock.lang.Specification
+
+import static org.gradle.testfixtures.ProjectBuilder.builder
+import static org.gradle.tooling.GradleConnector.newConnector
+
+class SolutionSpec extends Specification {
+
+    @Shared projectDir = new File('.')
+    @Shared projectConnection = newConnector().forProjectDirectory(projectDir).connect()
+
+    def cleanupSpec() {
+        projectConnection.close()
+    }
+
+    def 'test task is run and appropriate output is generated'() {
+        given:
+        def project = builder().withProjectDir(projectDir).build()
+        def connection = newConnector().forProjectDirectory(projectDir).connect()
+
+        when:
+        def output = runGradle(connection, 'test').toString('UTF-8').split('\n')
+
+        then:
+        output.size() == 2
+        def file = project.file("$project.buildDir/test-results/TEST-org.warsjawa.gradleWS.ExSpec.xml").absolutePath
+        ['FAILURE', 'SUCCESS'].every { output.contains("$file $it") }
+
+        cleanup:
+        connection.close()
+    }
+
+    def runGradle(ProjectConnection pc, String... tasks) {
+        def buildLauncher = pc.newBuild()
+        def output = new ByteArrayOutputStream()
+        buildLauncher.standardOutput = output
+        buildLauncher.forTasks(tasks).withArguments('-q', '-Dtest.single=ExSpec').run()
+        output
+    }
+}
